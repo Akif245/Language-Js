895c483f2a6dbadd429b48a0ee0939dce8834576
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-11 23:41:35 +0530

    Two Dimensional Array

---
+++ additionof2Darray.js
@@ -0,0 +1,20 @@
+// let Array = [
+//   [1, 2, 3],
+//   [4, 5, 6,8,8,8]
+// ];
+// let Array1 = [
+//   [0, 0, 0],
+//   [9, 9, 9]
+// ];
+// console.log(Array+","+Array1);
+                                                   //  or
+
+let arr = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9]
+];
+for (let a = 0; a < arr.length; a++) {
+  for (let b = 0; b < 5; b++) {
+    console.log(arr[a][b]);
+  }
+}
