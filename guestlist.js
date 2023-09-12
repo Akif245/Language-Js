1bcff5aa8b2a79c5c484bad9c083af6459891041
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-13 00:30:24 +0530

    includes syntax

---
+++ guestlist.js
@@ -0,0 +1,8 @@
+let a = prompt("Inter your name")
+let arr=["akif","mohammed","ahmed","abdul"]
+if (arr.includes(a)) {
+          alert("Hello " + a + " Welcome to the party.")          
+}
+else{
+          alert("Sorry you are not invited.")
+}
