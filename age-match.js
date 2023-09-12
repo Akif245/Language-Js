0dedd2066f2169cf939f70dcec408d22b2d77597
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-13 00:29:15 +0530

    age match

---
+++ age-match.js
@@ -0,0 +1,8 @@
+let a = prompt("Inter your age")
+let b = prompt("Inter your age")
+if (a === b) {
+          alert("you both can be friend")        
+}
+else{
+          alert("you cannot be friends ")
+}
