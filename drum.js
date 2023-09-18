ef1e2616dd1687287f307bb5fb82de202300aab8
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-19 01:42:55 +0530

    working drum

---
+++ drum.js
@@ -0,0 +1,6 @@
+var allbuttons = document.querySelectorAll(".a").length;
+for (let i = 0; i < allbuttons; i++) {
+  document.querySelectorAll(".a")[i].addEventListener("click", function () {
+    console.log("akif");
+  });
+}
