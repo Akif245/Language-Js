53c28a456ae25eee01e1e44879d4dd3f2652f69a
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-19 23:14:02 +0530

    new functions add-on

--- drum.js
+++ drum.js
@@ -1,6 +1,16 @@
 var allbuttons = document.querySelectorAll(".a").length;
-for (let i = 0; i < allbuttons; i++) {
-  document.querySelectorAll(".a")[i].addEventListener("click", function () {
-    console.log("akif");
-  });
-}
+
+// for (let i = 0; i < allbuttons; i++) {
+
+//   document.querySelectorAll(".a")[i].addEventListener("click", function () {
+
+//     console.log("akif");
+
+//   });
+// }
+
+document.querySelector("button").addEventListener("click",function(){
+  
+  var  audio = new Audio('audio.mp3');
+  audio.play()
+})
