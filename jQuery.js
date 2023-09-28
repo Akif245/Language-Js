4ceed3ed97c9181248cd687d9b97d56abaa46680
Mohammed Abdul Akif <139588023+Akif245@users.noreply.github.com>
2023-09-29 00:10:09 +0530

    jQuery{ $ } uses

---
+++ jQuery.js
@@ -0,0 +1,22 @@
+//  to add the class from CSS
+$("h1").addClass("title")
+
+
+// to remove the class from CSS
+$("h1").removeClass("title")
+
+
+
+// to ad multiple class (we should give a SPACE AND  can add the class[inside the double colin....]
+$("h1").addClass("title margin")
+// this is class( 1    )(   2)
+
+
+
+// to change the text
+$("button").text("Don't Click Me")      
+
+
+// to give HTML in      in will take HTML TAGS also
+$("button").html("<b>akif</b>")      
+
